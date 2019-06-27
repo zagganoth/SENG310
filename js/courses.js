3a5328947d48eaa2f77d91002b0cbbec65149901
Adam Hultman <adamhultman@uvic.ca>
2019-06-27 16:24:16 -0700

    Added sections in courses.js

--- js/courses.js
+++ js/courses.js
@@ -42,6 +42,22 @@ var courses =
                 "days": "MH",
                 "registered": false
                 
+            },
+            {
+                "section": "A02",
+                "prof": "Dr. D",
+                "duration": "12:30-14:20",
+                "days": "MH",
+                "registered": false
+                
+            },
+            {
+                "section": "A01",
+                "prof": "Dr. A",
+                "duration": "8:30-10:20",
+                "days": "MH",
+                "registered": false
+                
             }
         ],
         "labs": [
@@ -58,8 +74,22 @@ var courses =
                 "section": "A01",
                 "prof": "Dr. E",
                 "duration": "15:30-16:20",
-                "days": "MH",
+                "days": "TWF",
                 "registered": true
+            },
+            {
+                "section": "A02",
+                "prof": "Dr. F",
+                "duration": "10:30-11:20",
+                "days": "TWF",
+                "registered": false
+            },
+            {
+                "section": "A03",
+                "prof": "Dr. G",
+                "duration": "12:30-13:20",
+                "days": "TWF",
+                "registered": false
             }
         ],
         "labs": [
