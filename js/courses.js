7764e80e8db5992f14946862c4b6adef0cedbcbe
Anish Shenwai <ashenwai@uvic.ca>
2019-06-27 15:04:45 -0700

    Added new courses.js and staged changes for dynamically adding courses

---
+++ js/courses.js
@@ -0,0 +1,73 @@
+var courses =
+{
+    "CSC 226":
+    {
+        "sections": [
+            {
+                "section": "A01",
+                "prof": "Dr. A",
+                "duration": "9:30-10:20",
+                "days": "MWF",
+                "registered": false
+            },
+            {
+                "section": "A02",
+                "prof": "Dr. B",
+                "duration": "10:30-11:20",
+                "days": "MWF",
+                "registered": false
+            },
+            {
+                "section": "A03",
+                "prof": "Dr. C",
+                "duration": "15:30-16:50",
+                "days": "MH",
+                "registered": false
+            }
+        ],
+        "labs": [
+
+        ],
+        "tutorials": [
+
+        ]
+    },
+    "ECE 310":
+    {
+        "sections": [
+            {
+                "section": "A01",
+                "prof": "Dr. D",
+                "duration": "10:30-12:20",
+                "days": "MH",
+                "registered": false
+                
+            }
+        ],
+        "labs": [
+
+        ],
+        "tutorials": [
+
+        ]
+    },
+    "SENG 310":
+    {
+        "sections": [
+            {
+                "section": "A01",
+                "prof": "Dr. E",
+                "duration": "15:30-16:20",
+                "days": "MH",
+                "registered": true
+            }
+        ],
+        "labs": [
+
+        ],
+        "tutorials": [
+
+        ]
+    }
+
+}
--- js/registration.js
+++ js/registration.js
@@ -1,4 +1,4 @@
-$( function() {
+$(function () {
 	$( "#tabs" ).tabs();
 	csc = new CourseSection("CSC226","A01","Dr.A","9:30-10:20",'MWF');
 	ece310 = new CourseSection("ECE310","A01","Dr.B","10:30-11:50",'MH');
@@ -8,13 +8,30 @@ $( function() {
 	seng320 = new CourseSection("SENG320","A01","Dr.CD","14:30-16:20",'MH');
 	term1courses = [csc,ece310,seng310];
 	term2courses = [csc2,ece320,seng320];
-	tab1Table = createCourseTable(term1courses);
-	tab2Table = createCourseTable(term2courses);
-	$('#tabs-1').prepend(tab1Table);
-	$('#tabs-2').prepend(tab2Table);
-	$('#tabs-3').prepend(tab1Table);
-  } );
+    tab1Table = createCourseTable(term1courses,"Term1");
+    tab2Table = createCourseTable(term2courses, "Term2");
+    tabEmptyTable = createCourseTable([], "Term1");
 
+	$('#tabs-1').prepend(tabEmptyTable);
+	$('#tabs-2').prepend(tab2Table);
+    $('#tabs-3').prepend(tab1Table);
+    
+    $('body').on('click','.addCourse',function (event) {
+        console.log(event.target.innerText)
+        console.log(courses[event.target.innerText]);
+    })
+
+}
+);
+tab1CoursesShown = false;
+class Course
+{
+
+    constructor(name, sections) {
+        this.name = name;
+        this.sections = sections;
+    }
+}
 class CourseSection 
 {
 	constructor(name,sectionNumber,prof,duration,days,registered=false) {
@@ -29,8 +46,11 @@ class CourseSection
 		this.registered = registered;
 	}
 }
+function createSuggestionsDiv()
+{
 
-function createCourseTable(courses)
+}
+function createCourseTable(names,courses,termName)
 {
 	//Base HTML for table
 	var tableHTML = `
@@ -44,61 +64,79 @@ function createCourseTable(courses)
 				<th>Friday</th>
 			</tr>
 	`;
-	
-	//Find the first and last half-hours that will have classes in them.
-	var first = 100;
-	var last = -1;
-	for(var i in courses) {
-		if(courses[i].startTime < first) {
-			first = courses[i].startTime;
-		}
-		if(courses[i].endTime > last) {
-			last = courses[i].endTime;
-		}
-	}
-	
-	//For each possible time slot, check if a course is occuring during that timeslot
-	for(var halfHour = first; halfHour < last; halfHour++)
-	{
-		tableHTML += "<tr>";
-		var remainingDays = 6;
-		var cellsChanged = false;
-		//By default assume the table row is empty
-		var cells = ["<td></td>","<td></td>","<td></td>","<td></td>",'<td></td>'];
-		for(var index in courses)
-		{
-			//If a course starts at this half hour chunk, then for each day that it occurs, change the column to be that course
-			if(courses[index].startTime == halfHour)
-			{
-				var rowSpan = courses[index].endTime - courses[index].startTime;
-				for(ind in courses[index].days)
-				{
-					cellsChanged = true;
-					cells[courses[index].days[ind]] = "<td rowspan = " + rowSpan + (courses[index].registered ? " style='background-color:#CCC'" : '') + ">"+courses[index].name+"<br />"+courses[index].timePeriod+"</td>";
-				}
-			}
-			//If the course <td> has already been created, make sure another one isn't created in the same timeslot
-			else if(courses[index].startTime < halfHour && courses[index].endTime > halfHour)
-			{
-				for(ind in courses[index].days)
-				{
-					cellsChanged = true;
-					cells[courses[index].days[ind]] = "";
-				}
-			}
-		}
-		 
-		tableHTML += "<td>" + (Math.floor(halfHour / 2) + 8) + (halfHour % 2 == 0 ? ":00" : ":30") + "</td>" + cells.join('');
-		
-		
-		tableHTML += "</tr>";
-	}
-	tableHTML += "</table>";
-	//This is the "Selected Courses Shown Here" area, all made into one cell. We can add logic to display courses in here.
-	tableHTML += "<table><tr><th>Selected Courses</th></tr><tr><td rowspan = 30 style='vertical-align:top; float:left; width:100%; height:500px;'><div style='margin:10px;border:1px solid black'>Search</div></td></tr></table>";
-	return tableHTML;
-}
+    if (courses.length == 0) {
+        console.log("Hi");
+        tableHTML += "<tr><td colspan=6 style='height:500px'>Add some courses to begin</td></tr>";
+    }
+    else {
+        //Find the first and last half-hours that will have classes in them.
+        var first = 100;
+        var last = -1;
+        for (var i in courses) {
+            if (courses[i].startTime < first) {
+                first = courses[i].startTime;
+            }
+            if (courses[i].endTime > last) {
+                last = courses[i].endTime;
+            }
+        }
 
+        //For each possible time slot, check if a course is occuring during that timeslot
+        for (var halfHour = first; halfHour < last; halfHour++) {
+            tableHTML += "<tr>";
+            var remainingDays = 6;
+            var cellsChanged = false;
+            //By default assume the table row is empty
+            var cells = ["<td></td>", "<td></td>", "<td></td>", "<td></td>", '<td></td>'];
+            for (var index in courses) {
+                //If a course starts at this half hour chunk, then for each day that it occurs, change the column to be that course
+                if (courses[index].startTime == halfHour) {
+                    var rowSpan = courses[index].endTime - courses[index].startTime;
+                    for (ind in courses[index].days) {
+                        cellsChanged = true;
+                        cells[courses[index].days[ind]] = "<td rowspan = " + rowSpan + (courses[index].registered ? " style='background-color:#CCC'" : '') + ">" + courses[index].name + "<br />" + courses[index].timePeriod + "</td>";
+                    }
+                }
+                //If the course <td> has already been created, make sure another one isn't created in the same timeslot
+                else if (courses[index].startTime < halfHour && courses[index].endTime > halfHour) {
+                    for (ind in courses[index].days) {
+                        cellsChanged = true;
+                        cells[courses[index].days[ind]] = "";
+                    }
+                }
+            }
+
+            tableHTML += "<td>" + (Math.floor(halfHour / 2) + 8) + (halfHour % 2 == 0 ? ":00" : ":30") + "</td>" + cells.join('');
+
+
+            tableHTML += "</tr>";
+        }
+        tableHTML += "</table>";
+        //This is the "Selected Courses Shown Here" area, all made into one cell. We can add logic to display courses in here.
+        
+    }
+    tableHTML += "<table><th>Selected Courses</th></tr><tr><td rowspan = 30 style='vertical-align:top; float:left; width:100%; height:500px;'><div id='courseOptions_" + termName + "'><input style='margin:10px;border:1px solid black' placeholder='Search' onclick='showCourses(\"" + termName + "\"," + JSON.stringify(courses) + ")'></input></div></td></tr></table>";
+    return tableHTML;
+}
+function showCourses(term,courses) {
+    if (!tab1CoursesShown)
+    {
+        console.log(term);
+        console.log(courses);
+        var divHTML = `<table>
+                        <tr>
+                            <td class='addCourse `+term+`'>CSC 226</td>
+                        </tr>
+                        <tr>
+                            <td class='addCourse `+ term +`'>SENG 310</td>
+                        </tr>
+                      </table>`;
+        $('#courseOptions_' + term).append(divHTML);;
+        console.log(document.getElementById("courseOptions_" + term).innerHTML);
+        tab1CoursesShown = true;
+    }
+
+}
 function convertDurationToTimePeriod(duration)
 {
 	dur = duration.split('-');
